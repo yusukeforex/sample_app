283bda7537f972f9102d8916ce9a39ed64eec064
yusukeforex <momijina@icloud.com>
2019-10-11 17:54:42 +0900

    Implement basic login

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  get 'users/new'
+  # get 'users/new'
   # root_path -> '/'
   # root_url -> 'http://www.example.com/'
   root 'static_pages#home'
@@ -8,6 +8,9 @@ Rails.application.routes.draw do
   get '/contact', to: 'static_pages#contact'
   get '/signup', to: 'users#new'
   post '/signup', to: 'users#create'
+  get '/login', to: 'sessions#new'
+  post '/login', to: 'sessions#create'
+  delete '/logout', to: 'sessions#destroy'
   resources :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
--- test/integration/users_signup_test.rb
+++ test/integration/users_signup_test.rb
@@ -19,13 +19,14 @@ class UsersSignupTest < ActionDispatch::IntegrationTest
   test "valid signup information" do
     get signup_path
     assert_difference 'User.count', 1 do
-      post signup_path, params: { user: { name: "Example User",
+      post users_path, params: { user: { name: "Example User",
                                           email: "user@example.com",
                                           password: "password",
                                           password_confirmation: "password"}}
     end
     follow_redirect!
     assert_template 'users/show'
-    assert_not flash.empty?
+    assert is_logged_in?
   end
+
 end
